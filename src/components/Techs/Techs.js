7a3d39f3ffd0d1f767a8a8d4faae02bea47d5fc7
Olga Tananova <tananovao@yandex.ru>
2023-05-24 15:07:49 -0700

    fix: fixed bugs, edited README

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -4,7 +4,7 @@ function Techs() {
   return (<section className={'techs'}>
     <div className={'section section_type_techs'}>
       <h2 className={'section__heading section__heading_type_techs'}>Technology stack</h2>
-      <h3 className={'techs__subheading'}>7 technologies</h3>
+      <h3 className={'techs__subheading'}>8 technologies</h3>
       <p className={'techs__description'}> During the course, we studied the technologies
         {window.screen.width > 321 ? <br/>: null} to build the final project.</p>
       <ul className={'techs__list'}>
@@ -15,6 +15,7 @@ function Techs() {
         <li className={'tech__list-item'}>Git</li>
         <li className={'tech__list-item'}>Express.js</li>
         <li className={'tech__list-item'}>mongoDB</li>
+        <li className={'tech__list-item'}>Redux</li>
       </ul>
     </div>
   </section>)
